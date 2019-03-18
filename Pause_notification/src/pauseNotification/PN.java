1e43480354c108bdf73af3d85f48de2276103f02
Rax <captainrax@gmail.com>
2019-03-18 15:03:39 +0100

    i hecking changed a comment, fight me.

--- Pause_notification/src/pauseNotification/PN.java
+++ Pause_notification/src/pauseNotification/PN.java
@@ -90,7 +90,7 @@ public class PN {
 			public void run() {
 
 				LocalTime now = LocalTime.now(); // gets local time
-				String time = now.format(dtf); // converts formated time into String
+				String time = now.format(dtf); // converts formated time into Stringd
 				System.out.println(time); // debug
 
 				if (time.equals(timecomp)) {
